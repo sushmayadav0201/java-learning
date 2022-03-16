4663b84532382bb35ae22ea508fbd8d9d4323315
unknown <SushmaYadav0201>
2022-03-16 09:45:56 +0530

    my first

---
+++ chapter-01/hellosushma.java
@@ -0,0 +1,11 @@
+//this is my first java program
+public class hellosushma{
+	public static void main(String[] args) {
+		System.out.println("Hello, sushma!");
+
+
+
+
+	}
+
+}
---
+++ chapter-01/helloworld.java
@@ -0,0 +1,7 @@
+//this is my first java program
+public class hellosushma{
+	public static void main(String[] args) {
+		System.out.println("Hello, sushma!");
+	}
+
+}
